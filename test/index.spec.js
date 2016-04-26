35b41d3cdea1f70c3eb338bfa3285b155f589fa3
Wes Moberly <wesm87@gmail.com>
2016-04-25 20:54:59 -0400

    Update tests, add jsHint config, add jsDoc files

---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import chai from 'chai';
+import app  from '../app/';
+
+const assert = chai.assert;
+
+describe( 'WP Project Manager', () => {
+	it( 'should skip this test' );
+});
