af9f1e1e51735cd03904439e6c7798e81c8dd00e
Wes Moberly <wesm87@gmail.com>
2016-08-27 19:32:42 -0400

    Add some basic tests

---
+++ test/app.spec.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha */
+
+import chai from 'chai';
+import app from '../app/';
+
+const should = chai.should();
+
+describe( 'app', () => {
+
+	describe( 'index.js', () => {
+
+		it( 'should export a non-empty object', () => {
+			should.exist( app );
+			app.should.be.an( 'object' );
+		} );
+
+	} );
+
+} );
---
+++ test/commands.spec.js
@@ -0,0 +1,51 @@
+/* eslint-env mocha */
+
+import chai from 'chai';
+
+const should = chai.should();
+
+const basePath = '../app/commands';
+
+describe( 'commands', () => {
+
+	const commands = [
+		'config.create',
+		'config.display',
+		'deps.install',
+		'plugin.create-tests',
+		'plugin.create',
+		'project.create',
+		'theme.create-tests',
+		'theme.create',
+		'wp.install',
+	];
+
+	for ( const command of commands ) {
+
+		describe( command, () => {
+
+			const commandModule = require( `${ basePath }/${ command }` );
+
+			it( 'should export a non-empty object', () => {
+				should.exist( commandModule );
+				commandModule.should.be.an( 'object' );
+			} );
+
+			it( 'should export a `command` property', () => {
+				commandModule.should.have.property( 'command' );
+			} );
+
+			it( 'should export a `describe` property', () => {
+				commandModule.should.have.property( 'describe' );
+			} );
+
+			it( 'should export a `builder` property', () => {
+				commandModule.should.have.property( 'builder' );
+			} );
+
+			it( 'should export a `handler()` method', () => {
+				commandModule.should.respondTo( 'handler' );
+			} );
+		} );
+	}
+} );
